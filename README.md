f32704e0d50cfc4371f219a834b998a0e54279db
David Clark <david.clark@ntlworld.com>
2018-12-28 10:07:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+My code from the Pluralsight course, React, getting started.
---
+++ src/button.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+
+const Button = function() {
+  return <button>Go</button>;
+};
+export default Button; // Don’t forget to use export default!
